def79b07c1d5e849603272a946e916afb194b09a
decentguy <bludd1one@gmail.com>
2019-04-18 11:54:25 +0200

    fixed the user bug.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,38 @@
 /** Michael: codeblock starts**/
-let userLevel = document.getElementById('levelInput').value;
+// let userLevel = document.getElementById('levelInput').value;
+// let userLevel = 0;
 
-function createArray(){
+//  if(userLevel > 0) {
+//      document.getElementById('levelInput').value = "";
+//      let userLevel = document.getElementById('levelInput').value;
+
+//      console.log(userLevel);
+//      // return userLevel;
+//  }
+function resetCards() {
+    var userLevel = document.getElementById("levelInput").value;
+
+    let getLength = document.querySelectorAll("#memory-game");
+
+    if (getLength.length < 1) {
+        loadCards();
+    } else {
+        var getSection = document.getElementById("memory-game");
+
+        getSection.parentNode.removeChild(getSection);
+
+        loadCards();
+    }
+}
+
+function createArray() {
+    let userLevel = document.getElementById("levelInput").value;
+
     if (userLevel >= 2 && userLevel < 13 && userLevel % 2 === 0) {
         const fillRange = (start, end) => {
-        return Array(end - start + 1).fill().map((item, index) => start + index);
+            return Array(end - start + 1)
+                .fill()
+                .map((item, index) => start + index);
         };
 
         var makeArray = fillRange(0, userLevel - 1);
@@ -13,32 +41,17 @@ function createArray(){
     };**/
 
     return makeArray;
-};
+}
 
-function resetCards () {
-    var getLength = document.querySelectorAll("#memory-game");
-
-    if(getLength.length < 1){
-        loadCards();
-    } 
-    else {
-        
-        var getSection = document.getElementById("memory-game");
-
-        getSection.parentNode.removeChild(getSection);
-
-        loadCards();
-    };  
-}; 
 /** Michael: codeblock ends**/
 
-function loadCards()
-{
-  //  var getSection = document.getElementById("memory-game");
- // getSection.parentNode.removeChild(getSection);
-
-        /**Michael: added the array created dynamically with users input**/
-        let nums = createArray(),
+function loadCards() {
+    //  var getSection = document.getElementById("memory-game");
+    // getSection.parentNode.removeChild(getSection);
+    let userLevel = document.getElementById("levelInput").value;
+    /**Michael: added the array created dynamically with users input**/
+    console.log(createArray());
+    let nums = createArray(),
         ranNums = [],
         i = nums.length,
         j = 0;
@@ -52,12 +65,11 @@ function loadCards()
     * */
 
     /*******   ranNums   *******/
-    while (i--) 
-    {
-        j = Math.floor(Math.random() * (i+1));
+    while (i--) {
+        j = Math.floor(Math.random() * (i + 1));
         ranNums.push(nums[j]);
-        nums.splice(j,1);
-    };
+        nums.splice(j, 1);
+    }
 
     /*
     Create an HTML section and add the class
@@ -68,14 +80,25 @@ function loadCards()
     let section = document.createElement("section");
     section.classList.add("memory-game");
 
-
     /*
     store the filenames of all the images that will
     be used in the game in an array.
     **/
 
-    let cards = ["img/U_black.png", "img/U_black.png", "img/M.jpeg", "img/M.jpeg", "img/U_orange.jpeg",
-        "img/U_orange.jpeg", "img/Z.jpg", "img/Z.jpg", "img/I.jpeg", "img/I.jpeg", "img/umuzi.png", "img/umuzi.png"];
+    let cards = [
+        "img/U_black.png",
+        "img/U_black.png",
+        "img/M.jpeg",
+        "img/M.jpeg",
+        "img/U_orange.jpeg",
+        "img/U_orange.jpeg",
+        "img/Z.jpg",
+        "img/Z.jpg",
+        "img/I.jpeg",
+        "img/I.jpeg",
+        "img/umuzi.png",
+        "img/umuzi.png"
+    ];
     console.log("WE're about to go in...");
 
     /**Michael: set card length to the value of the users input.**/
@@ -84,10 +107,9 @@ function loadCards()
     /*
     for-loop to load and shuffle cards.
     **/
-    for (let index = 0; index < cards.length; index++) 
-    {
+    for (let index = 0; index < cards.length; index++) {
         /** create a div then add to the div; a class named memory-card as well as a dataset of the card's filename **/
-        let cardDiv = document.createElement('div');
+        let cardDiv = document.createElement("div");
         cardDiv.classList.add("memory-card");
         cardDiv.dataset.card = cards[ranNums[index]];
 
@@ -108,19 +130,25 @@ function loadCards()
         /** make the meomry-card div a child of the memory-game div **/
         section.appendChild(cardDiv);
     }
-    
+
     /** once all cards have been loaded, add div memory-game a child of section **/
     document.body.appendChild(section);
     console.log(cards);
 
-    cardSelector()
+    cardSelector();
 
-    var sectionId = document.getElementsByTagName("section")[0].setAttribute("id", "memory-game");
-};
+    var sectionId = document
+        .getElementsByTagName("section")[0]
+        .setAttribute("id", "memory-game");
+    // nums = [];
+    // console.log(nums);
+    // userLevel = "";
+    // console.log(userLevel + "dumela")
+    // userLevel = document.getElementById('levelInput').value;
+}
 
 /** get all elements in the documents with a class name of memory-card and store in cards variable **/
 //const cards = document.querySelectorAll('.memory-card');
-
 
 /*
 -lock variable to lock the board during wait period between turns as well as to prevent clicking a card twice in a turn.
@@ -132,29 +160,26 @@ let lock = false;
 let flipped = false;
 let first, second;
 
-function flipCard() 
-{
+function flipCard() {
     /** exit function if more than two cards are clicked in a turn **/
 
-    if (lock)
-        return;
+    if (lock) return;
 
     /** add the flip class to the div of the card that's been clicked. **/
 
-    this.classList.toggle('flip');
-
+    this.classList.toggle("flip");
 
     /** if current card is first card; add the card's div to 'this' **/
 
-    if (!flipped)
-    {
+    if (!flipped) {
         flipped = true;
         first = this;
-
-    } else { /** exit function if a card is clicked more than once in a turn **/
-        if (this === first){
+    } else {
+        /** exit function if a card is clicked more than once in a turn **/
+        if (this === first) {
             [flipped, first] = [null, null];
-            return;}
+            return;
+        }
 
         /** if current card is second card, reset flipped variable and set second to 'this' **/
         flipped = false;
@@ -163,22 +188,21 @@ function flipCard()
         /** if cards match in a turn, remove eventListener from those cards **/
 
         console.log(first.dataset.card + " vs " + second.dataset.card);
-        if (first.dataset.card === second.dataset.card)
-        {
+        if (first.dataset.card === second.dataset.card) {
             console.log("They're a match!!!");
-            first.removeEventListener('click', flipCard);
-            second.removeEventListener('click', flipCard);
-        }else {/** if cards are not a match in a turn, remove the flip class from them. **/
+            first.removeEventListener("click", flipCard);
+            second.removeEventListener("click", flipCard);
+        } else {
+            /** if cards are not a match in a turn, remove the flip class from them. **/
             lock = true;
-            setTimeout(() =>{
-            first.classList.remove('flip');
-            second.classList.remove('flip');
+            setTimeout(() => {
+                first.classList.remove("flip");
+                second.classList.remove("flip");
 
-            /** reset our tracking variables at the end of the turn **/
-            [flipped, lock] = [false, false];
-            [first, second] = [null, null];
-
-        }, 900);
+                /** reset our tracking variables at the end of the turn **/
+                [flipped, lock] = [false, false];
+                [first, second] = [null, null];
+            }, 900);
         }
     }
 }
@@ -187,7 +211,9 @@ function flipCard()
 
 /** attach a click event to all divs we stored in cards variable and run flipcard function on that div if it's clicked on **/
 
- function cardSelector(){
-    document.querySelectorAll('.memory-card').forEach(card => card.addEventListener('click', flipCard));
+function cardSelector() {
+    document
+        .querySelectorAll(".memory-card")
+        .forEach(card => card.addEventListener("click", flipCard));
     console.log("I'm here.");
-};
+}
